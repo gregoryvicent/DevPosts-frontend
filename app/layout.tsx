7460a9a0ddf4b2d469d244e221b5f78e6a2095ab
Gregory Vicent <gregoryvicent.dev@gmail.com>
2023-09-27 16:43:34 -0400

    004: Start with Home Page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,20 @@
+import './globals.css' // Tailwindcss styles import
+
+import Header from '../components/Header'
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="es">
+	<body className='bg-gradient-to-b from-blue-600 from-5% via-slate-950 via-40% to-slate-950 to-90% h-screen overflow-x-hidden'>
+	    <Header />
+	    <main>
+		{children}
+	    </main>
+	</body>
     </html>
   )
 }
---
+++ components/Header/index.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+	<header className="bg-slate-950 w-auto h-14 m-2 rounded-full">
+	    <h1>This is a Header</h1>
+	</header>
+    )
+}
